fc9c82608d8afdadccb8986e9e0d4ab904572412
recordy <vladpopk@gmail.com>
2023-12-28 15:50:27 +0300

    базовый скрипт игрока

---
+++ Assets/Scripts/RECORDY/playerScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public static float playerHP;//baseValue=10;
+    public static float movSpeed;//baseValue=1.2;
+    public static float charmCooldown;//baseValue=10(seconds);
+    public static float charmRange;//baseValue=5(meters);
+    public static float summonBonusHP;//baseValue=5;
+    public static float charmCooldownStone;
+    public static float charmRangeStone;
+    public static float summonBonusHPStone;
+}
